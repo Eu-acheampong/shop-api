af1cea0bc4d245b63c4bb8a267cc189a198d1771
Eu-acheampong <evaamoafoa@gmail.com>
2022-04-21 14:00:27 +0000

    authentication

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { dbConnect } = require("./config/dbConnect");
+const authRouter = require("./routes/auth.router");
 const productRouter = require("./routes/product.router");
 
 const app = express();
+app.use(express.json());
+
+app.use("/auth", authRouter);
+app.use("/products", productRouter);
 
 const start = async () => {
   await dbConnect();
